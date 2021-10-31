8aa38619e5eef5e9cb7deab4df5843d78844349f
iaprofessional <75013595+iaprofessional@users.noreply.github.com>
2021-10-31 19:27:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+img = ""
+Status = ""
+objects = [];
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting..."
+}
+function modelLoaded() {
+    console.log("ml5 has sucessfully loaded");
+    Status = true;
+    objectDetector.detect(img, gotResult);
+}
+function gotResult(error, results) {
+ if (error) {
+  console.log(error);
+ } else {
+     console.log(results);
+     objects = results;
+ }
+}
+function preload() {
+    img = loadImage('dog_cat.jpg');
+}
+function draw() {
+    image(img, 0, 0, 640, 420);
+    if(Status != "") {
+     for (i = 0; i < objects.length; i++) {
+         document.getElementById("status").innerHTML = "Status: We have detected the objects below";
+
+         fill("#FF0000");
+         percent = floor(objects[i].confidence * 100);
+         text(objects[i].label + "" + percent + "%", objects[i].x, objects[i].y);
+         noFill();
+         stroke("#FF0000");
+         rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height,);
+     }
+    }
+}
